180b7d241eebdedbc2fa604d2643b6706bf61a54
Jakub Sosna <jakub.sosna@icloud.com>
2020-11-07 16:42:50 +0100

    Initial commit

---
+++ Assets/Scripts/BuildingSystem.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSystem : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera playerCamera;
+
+    private bool buildModeOn = false;
+    private bool canBuild = false;
+
+    private BlockSystem blockSystem;
+
+    [SerializeField]
+    private LayerMask buildableSurfacesLayer;
+
+    private Vector3 buildPos;
+
+    private GameObject currentTemplateBlock;
+
+    [SerializeField]
+    private GameObject blockTemplatePrefab;
+    [SerializeField]
+    private GameObject blockPrefab;
+
+    [SerializeField]
+    private Material templateMaterial;
+
+    private int blockSelectCounter = 0;
+
+    private void Start()
+    {
+        blockSystem = GetComponent<BlockSystem>();
+    }
+
+    private void Update()
+    {
+        // change to pick blocks with keys (1-9) or scroll
+        if (Input.GetKeyDown("1")) buildModeOn = !buildModeOn;
+
+        if (Input.GetKeyDown("r"))
+        {
+            blockSelectCounter++;
+            if (blockSelectCounter >= blockSystem.allBlocks.Count) blockSelectCounter = 0;
+        }
+
+        if (buildModeOn)
+        {
+            RaycastHit buildPosHit;
+
+            if (Physics.Raycast(playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)), out buildPosHit, 10, buildableSurfacesLayer))
+            {
+                Vector3 point = buildPosHit.point;
+                buildPos = new Vector3(Mathf.Round(point.x), Mathf.Round(point.y)+1/2f, Mathf.Round(point.z));
+                canBuild = true;
+            }
+            else
+            {
+                if(currentTemplateBlock != null)
+                    Destroy(currentTemplateBlock.gameObject);
+                canBuild = false;
+            }
+        }
+
+        if (!buildModeOn && currentTemplateBlock != null)
+        {
+            Destroy(currentTemplateBlock.gameObject);
+            canBuild = false;
+        }
+
+        if (canBuild && currentTemplateBlock == null)
+        {
+            currentTemplateBlock = Instantiate(blockTemplatePrefab, buildPos, Quaternion.identity);
+            currentTemplateBlock.GetComponent<MeshRenderer>().material = templateMaterial;
+        }
+
+        if (canBuild && currentTemplateBlock != null)
+        {
+            currentTemplateBlock.transform.position = buildPos;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceBlock();
+            }
+        }
+    }
+
+    private void PlaceBlock()
+    {
+        GameObject newBlock = Instantiate(blockPrefab, buildPos, Quaternion.identity);
+        Block tempBlock = blockSystem.allBlocks[blockSelectCounter];
+        newBlock.name = tempBlock.blockName + "-Block";
+        newBlock.GetComponent<MeshRenderer>().material = tempBlock.blockMaterial;
+    }
+}
---
+++ Assets/Scripts/Snapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Snapper : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 snap;
+        snap.x = Mathf.RoundToInt(transform.position.x - .5f);
+        snap.y = Mathf.RoundToInt(transform.position.y - .5f);
+        snap.z = Mathf.RoundToInt(transform.position.z - .5f);
+
+        transform.position = new Vector3(snap.x + .5f, snap.y + .5f, snap.z + .5f);
+    }
+}
