b589a42f28cb9f29fb178c007d9643863be94820
Jakub Sosna <jakub.sosna@icloud.com>
2020-11-18 18:32:41 +0100

    Add FPS display

---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    public Text fpsText;
+    private float dt;
+
+    void Update()
+    {
+        dt += (Time.deltaTime - dt) * 0.1f;
+        float fps = 1.0f / dt;
+        fpsText.text = "FPS " + Mathf.Ceil(fps).ToString();
+    }
+}
