5b31e1d0c5b52b476b57c350c3f47282cdb3214b
Jakub Sosna <jakub.sosna@icloud.com>
2021-02-01 18:19:58 +0100

    Implement tool sway

---
+++ Assets/Scripts/ToolSway.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolSway : MonoBehaviour
+{
+    [SerializeField]
+    private float amount;
+
+    [SerializeField]
+    private float maxAmount;
+
+    [SerializeField]
+    private float smoothAmount;
+
+    private Vector3 initialPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = -Input.GetAxis("Mouse X") * amount;
+        float movementY = -Input.GetAxis("Mouse Y") * amount;
+
+        movementX = Mathf.Clamp(movementX, -maxAmount, maxAmount);
+        movementY = Mathf.Clamp(movementY, -maxAmount, maxAmount);
+
+        Vector3 finalPosition = new Vector3(movementX, movementY);
+        transform.localPosition = Vector3.Lerp(
+            transform.localPosition, 
+            finalPosition + initialPosition, 
+            Time.deltaTime * smoothAmount
+            );
+    }
+}
